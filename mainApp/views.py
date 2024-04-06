7cb5a19ac0959f4e7e0e45e3856ac8dba4f8e8f0
akramjonovash02 <165456614+AkramjonovaSh02@users.noreply.github.com>
2024-04-06 10:11:25 +0500

    Update views.py

--- mainApp/views.py
+++ mainApp/views.py
@@ -6,10 +6,4 @@ class Home(View):
         return render(request, "index.html")
 
 
-class About(View):
-    def get(self, request):
-        return render(request, "about.html")
 
-class Tryouts(View):
-    def get(self, request):
-        return render(request, "tryouts.html")
