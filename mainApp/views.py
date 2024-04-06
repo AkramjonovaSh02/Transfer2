f8897e0a52e969a9380526cf19db2bdc80d2ca4b
akramjonovash02 <165456614+AkramjonovaSh02@users.noreply.github.com>
2024-04-06 10:23:13 +0500

    first

--- mainApp/views.py
+++ mainApp/views.py
@@ -5,5 +5,11 @@ class Home(View):
     def get(self, request):
         return render(request, "index.html")
 
+class About(View):
+    def get(self, request):
+        return render(request, "about.html")
 
+class Tryouts(View):
+    def get(self, request):
+        return render(request, "tryouts.html")
 
